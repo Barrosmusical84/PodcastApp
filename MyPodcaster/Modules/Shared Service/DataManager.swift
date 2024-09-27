0946bf95eed0c5e2f9fcd03e4f2b76f3d256a9cc
André Barros <barrosmusical@gmail.com>
2024-09-27 12:28:42 -0300

    created data manager

---
+++ MyPodcaster/Modules/Shared Service/DataManager.swift
@@ -0,0 +1,105 @@
+import Foundation
+
+class DataManager: NSObject, XMLParserDelegate {
+    var rssItems: [RSSItem] = []
+    var currentElement = ""
+    var currentTitle = ""
+    var currentLink = ""
+    var currentPubDate = ""
+    var currentImageURL = ""
+    var summary: String = ""
+    var duration: Int?
+
+    var completion: (([RSSItem]) -> ())?
+
+    func fetchRSSFeed(url: String) {
+        guard let feedURL = URL(string: url) else { return }
+
+        let task = URLSession.shared.dataTask(with: feedURL) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Error fetching data: \(String(describing: error))")
+                return
+            }
+
+            self.parseXML(data: data)
+        }
+
+        task.resume()
+    }
+
+    func parseXML(data: Data) {
+        let parser = XMLParser(data: data)
+        parser.delegate = self
+        parser.parse()
+    }
+
+    // XMLParserDelegate methods
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+
+        currentElement = elementName
+
+        switch currentElement {
+            case "item":
+                currentTitle = ""
+                currentLink = ""
+                currentPubDate = ""
+
+
+            case "itunes:image":
+                if let urlString = attributeDict["href"] {
+                    currentImageURL = urlString
+                }
+
+            default:
+                break
+        }
+    }
+
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        switch currentElement {
+            case "title":
+                currentTitle += string
+            case "link":
+                currentLink += string
+            case "pubDate":
+                currentPubDate += string
+                currentLink += string
+            case "itunes:summary":
+                summary += string
+            case "itunes:duration":
+                if let duration = Int(string) {
+                    self.duration = duration
+                }
+
+            default:
+                break
+        }
+    }
+
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "item" {
+            let item = RSSItem(
+                title: currentTitle,
+                link: currentLink,
+                pubDate: currentPubDate,
+                imageURL: URL(string: currentImageURL),
+                summary: nil,
+                duration: duration
+            )
+            rssItems.append(item)
+        }
+    }
+
+    func parserDidEndDocument(_ parser: XMLParser) {
+        completion?(rssItems)
+    }
+}
+
+struct RSSItem {
+    let title: String
+    let link: String
+    let pubDate: String
+    var imageURL: URL?
+    var summary: String?
+    var duration: Int?
+}
